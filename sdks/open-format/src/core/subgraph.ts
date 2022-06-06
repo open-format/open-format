10bcc6237a1c563ccddd7aee4a5b7ede24c69edf
Benjamin Reid <ben@simpleweb.co.uk>
2022-06-06 17:28:33 +0100

    Pass typescript variables and add docs for raw

--- sdks/open-format/src/core/subgraph.ts
+++ sdks/open-format/src/core/subgraph.ts
@@ -1,10 +1,32 @@
-import { request, RequestDocument } from 'graphql-request';
+import { request, RequestDocument, Variables } from 'graphql-request';
 
 const ENDPOINT =
   'https://api.thegraph.com/subgraphs/name/simpleweb/open-format';
 
-async function raw(document: RequestDocument) {
-  return await request(ENDPOINT, document);
+/**
+ * Makes a request to the Open Format subgraph
+ * @param document
+ * @returns a result from the subgraph
+ *
+ * @example
+ * ```
+ * raw(gql`
+ *  {
+     tokens {
+       id
+       saleData {
+         maxSupply
+       }
+     }
+    }
+ * `)
+ * ```
+ */
+async function raw<T = any, V = Variables>(
+  document: RequestDocument,
+  variables?: V
+) {
+  return await request<T, V>(ENDPOINT, document, variables);
 }
 
 export default {
