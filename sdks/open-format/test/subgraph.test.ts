771a611903dba0e3b4f9cacebc172482d2428dc0
Benjamin Reid <ben@simpleweb.co.uk>
2022-06-06 17:13:47 +0100

    Test the raw request

---
+++ sdks/open-format/test/subgraph.test.ts
@@ -0,0 +1,20 @@
+import { OpenFormatSDK } from '../src/index';
+import { gql } from 'graphql-request';
+
+const sdk = new OpenFormatSDK();
+
+describe('subgraph', () => {
+  it('allows you to perform a raw request', async () => {
+    const query = gql`
+      {
+        tokens {
+          id
+        }
+      }
+    `;
+
+    const result = await sdk.subgraph.raw(query);
+
+    expect(typeof result.tokens[0].id).toBe('string');
+  });
+});
