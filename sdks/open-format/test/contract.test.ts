d112e75672ab686e67d5604b25919d7ac1c643ff
Benjamin Reid <ben@simpleweb.co.uk>
2022-06-08 13:42:53 +0100

    Run ganache server before test

--- sdks/open-format/test/contract.test.ts
+++ sdks/open-format/test/contract.test.ts
@@ -1,6 +1,39 @@
+import ganache from 'ganache';
 import { OpenFormatSDK } from '../src/index';
 
 const sdk = new OpenFormatSDK();
+let server: ReturnType<typeof ganache.server>;
+
+beforeAll(() => {
+  return new Promise<void>((resolve, reject) => {
+    server = ganache.server({
+      logging: {
+        quiet: true,
+      },
+      wallet: {
+        accounts: [
+          {
+            balance: 1000000000000000000000,
+            secretKey:
+              '0xc27786e23ac741aceef158731965a6285f350e114952201baad6149c18d735e7',
+          },
+        ],
+      },
+    });
+
+    server.listen(8545, async err => {
+      if (err) {
+        reject();
+      }
+
+      resolve();
+    });
+  });
+});
+
+afterEach(() => {
+  server.close();
+});
 
 describe('contract', () => {
   it('deploys a contract', async () => {
