b3dfd592969b05abebdc0d5c008d0fe1bce58cf1
Benjamin Reid <ben@simpleweb.co.uk>
2022-06-09 12:00:46 +0100

    Tests for deploying with signer and private key

--- sdks/open-format/test/contract.test.ts
+++ sdks/open-format/test/contract.test.ts
@@ -31,25 +31,43 @@ beforeAll(() => {
   });
 });
 
-afterEach(() => {
+afterAll(() => {
   server.close();
 });
 
-const sdk = new OpenFormatSDK({
-  network: 'http://localhost:8545',
-  signer: new ethers.Wallet(
-    '0xc27786e23ac741aceef158731965a6285f350e114952201baad6149c18d735e7',
-    new ethers.providers.JsonRpcProvider('http://localhost:8545')
-  ),
-});
-
 describe('contract', () => {
-  it('deploys a contract', async () => {
+  it('deploys a contract with a signer', async () => {
+    const sdk = new OpenFormatSDK({
+      network: 'http://localhost:8545',
+      signer: new ethers.Wallet(
+        '0xc27786e23ac741aceef158731965a6285f350e114952201baad6149c18d735e7',
+        new ethers.providers.JsonRpcProvider('http://localhost:8545')
+      ),
+    });
+
     const receipt = await sdk.deploy({
       maxSupply: 100,
       mintingPrice: '0.01',
       name: 'Test',
       symbol: 'TEST',
+      url: 'ipfs://',
+    });
+
+    expect(receipt.status).toBe(1);
+  });
+
+  it('deploys with a private key', async () => {
+    const sdk = new OpenFormatSDK({
+      network: 'http://localhost:8545',
+      signer:
+        '0xc27786e23ac741aceef158731965a6285f350e114952201baad6149c18d735e7',
+    });
+
+    const receipt = await sdk.deploy({
+      maxSupply: 100,
+      mintingPrice: '0.01',
+      name: 'Test 1',
+      symbol: 'TEST1',
       url: 'ipfs://',
     });
 
