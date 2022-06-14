0b6d791db448b7552a8560984ed898ca598672a1
Benjamin Reid <ben@simpleweb.co.uk>
2022-06-14 11:16:03 +0100

    Try out isloading in example

--- examples/react-next/pages/index.tsx
+++ examples/react-next/pages/index.tsx
@@ -2,14 +2,18 @@ import { NextPage } from "next";
 import { useSaleData } from "@simpleweb/open-format-react";
 
 const Home: NextPage = () => {
-  const { data } = useSaleData({
+  const { data, isLoading } = useSaleData({
     tokenId: "0x021d35cd4849596f1013cf92f718ec7bf5541bc2"
   });
 
   return (
     <div>
       <h1>Open Format React</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      )}
     </div>
   );
 };
