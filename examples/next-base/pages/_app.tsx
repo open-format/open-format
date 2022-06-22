c56c330988e7ed0d2c53350cc446be4ad0b974fa
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-06-22 11:37:33 +0100

    initial wrapper not working correctly

--- examples/next-base/pages/_app.tsx
+++ examples/next-base/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { OpenFormatProvider } from "@simpleweb/open-format-react";
+import { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <OpenFormatProvider config={{ network: "mumbai" }}>
+        <Component {...pageProps} />
+      </OpenFormatProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
