5264e8ba97a1991482c18a5efa4fe318a111af1c
Benjamin Reid <ben@simpleweb.co.uk>
2022-07-08 15:16:02 +0100

    Setup signer for tests

--- sdks/react/src/utilities.tsx
+++ sdks/react/src/utilities.tsx
@@ -1,6 +1,12 @@
 import React, { FC, ReactElement } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
 import { OpenFormatProvider } from '../src/provider';
+import { ethers } from 'ethers';
+
+const signer = new ethers.Wallet(
+  '0x04c65fb1737cf9a5fb605b403b5027924309e53a3433d06029a0441cc03e2042',
+  new ethers.providers.JsonRpcProvider('http://localhost:8545')
+);
 
 const App: FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
@@ -8,6 +14,7 @@ const App: FC<{ children: React.ReactNode }> = ({ children }) => {
       config={{
         network: 'localhost',
         factory: '1fe0b323-223f-48aa-8797-137931473f49',
+        signer,
       }}
     >
       {children}
