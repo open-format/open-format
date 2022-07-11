efd15462f90c84eed15604e8c9508aee1824618d
Craig Coles <craig@simpleweb.co.uk>
2022-07-11 14:32:50 +0100

    Adds in test for configuration options

--- sdks/react/test/useRawRequest.test.tsx
+++ sdks/react/test/useRawRequest.test.tsx
@@ -22,10 +22,45 @@ describe('useRawRequest', () => {
       '0x0667c3fa16ea85166e1d7fbe6da14031c6b541a1'
     );
   });
+
+  it('allows you to pass variables to a query', async () => {
+    render(<TestConfig />);
+
+    await waitFor(() => {
+      screen.getByTestId('tokenId');
+      screen.getByTestId('success');
+    });
+
+    expect(screen.getByTestId('tokenId')).toHaveTextContent(/0x/);
+    expect(screen.getByTestId('success')).toBeVisible();
+  });
 });
 
+function TestConfig() {
+  const [showSuccess, setShowSuccess] = React.useState(false);
+  const { data } = useRawRequest<any, any, any>({
+    query: gql`
+      {
+        tokens {
+          id
+        }
+      }
+    `,
+    config: {
+      onSuccess: () => setShowSuccess(true),
+    },
+  });
+
+  return (
+    <>
+      {data && <span data-testid="tokenId">{data?.tokens?.[0]?.id}</span>}
+      {showSuccess && <span data-testid="success">Success!</span>}
+    </>
+  );
+}
+
 function TestVariables() {
-  const { data } = useRawRequest({
+  const { data } = useRawRequest<any, any, any>({
     query: gql`
       query GetToken($id: String!) {
         token(id: $id) {
@@ -42,7 +77,7 @@ function TestVariables() {
 }
 
 function Test() {
-  const { data } = useRawRequest({
+  const { data } = useRawRequest<any, any, any>({
     query: gql`
       {
         tokens {
