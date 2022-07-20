31e317c350f40136c08c21f547e6147b1266841e
Benjamin Reid <ben@simpleweb.co.uk>
2022-07-20 17:15:27 +0100

    Setup withdraw test with SDK

--- sdks/react/test/useWithdrawTokenFunds.test.tsx
+++ sdks/react/test/useWithdrawTokenFunds.test.tsx
@@ -1,138 +1,99 @@
+import { OpenFormatSDK } from '@simpleweb/open-format';
 import '@testing-library/jest-dom';
 import { ethers } from 'ethers';
 import React from 'react';
-import {
-  useMint,
-  useNFT,
-  useRevenueSharingAllocation,
-  useSetupRevenueSharing,
-  useWithdrawTokenFunds,
-} from '../src/hooks';
-import { DeployedTest, render, screen, waitFor } from '../src/utilities';
+import { useNFT, useWithdrawTokenFunds } from '../src/hooks';
+import { render, screen, waitFor } from '../src/utilities';
 
-function Withdraw({ address }: { address: string }) {
-  const nft = useNFT(address);
-  const { mint } = useMint(nft);
-  const { withdraw, data: withdrawData } = useWithdrawTokenFunds(nft);
+let address: string;
 
-  const account = new ethers.Wallet(
-    '0xc27786e23ac741aceef158731965a6285f350e114952201baad6149c18d735e7',
-    new ethers.providers.JsonRpcProvider('http://localhost:8546')
-  );
+describe('useWithdrawTokenFunds', () => {
+  beforeEach(async () => {
+    const sdk = new OpenFormatSDK({
+      network: 'http://localhost:8546',
+      signer: new ethers.Wallet(
+        '0x04c65fb1737cf9a5fb605b403b5027924309e53a3433d06029a0441cc03e2042',
+        new ethers.providers.JsonRpcProvider('http://localhost:8546')
+      ),
+    });
 
-  const onWithdraw = async () => {
-    await mint();
+    const { contractAddress } = await sdk.deploy({
+      maxSupply: 100,
+      mintingPrice: 0.01,
+      name: 'Test',
+      symbol: 'TEST',
+      url: 'ipfs://',
+    });
 
-    await account.sendTransaction({
+    address = contractAddress;
+
+    const nft = sdk.getNFT(contractAddress);
+
+    const account = new ethers.Wallet(
+      '0xc27786e23ac741aceef158731965a6285f350e114952201baad6149c18d735e7',
+      new ethers.providers.JsonRpcProvider('http://localhost:8546')
+    );
+
+    await nft.setupRevenueSharing({
+      revShareExtensionAddress: '0x483C3aDD26C87d2F99DcCB84Cbf61844B6aeD212',
+      collaborators: [
+        {
+          address: '0xee4abd006630aea6fa3e685c99506db31c09b3f4',
+          share: 1000,
+        },
+        {
+          address: '0x21b2be9090d1d319e57b67c4b5d37bc5ec29a9d0',
+          share: 1000,
+        },
+      ],
+      holderPercentage: 5000,
+    });
+
+    await nft.allocateRevenueShares([
+      {
+        address: '0xee4abd006630aea6fa3e685c99506db31c09b3f4',
+        share: 500,
+      },
+      {
+        address: '0x21b2be9090d1d319e57b67c4b5d37bc5ec29a9d0',
+        share: 500,
+      },
+    ]);
+
+    await nft.mint();
+
+    const tx = await account.sendTransaction({
       to: nft.address,
       value: '500',
     });
 
-    withdraw(0);
-  };
+    await tx.wait();
+  });
 
-  return (
-    <>
-      <button data-testid="withdraw" onClick={onWithdraw}>
-        Withdraw Token Funds
-      </button>
-      {withdrawData && <span data-testid="withdrawal"></span>}
-    </>
-  );
-}
+  function Withdraw() {
+    const nft = useNFT(address);
+    const { withdraw, data } = useWithdrawTokenFunds(nft);
 
-function Allocation({ address }: { address: string }) {
-  const nft = useNFT(address);
-  const { allocate, data: allocationData } = useRevenueSharingAllocation(nft);
+    return (
+      <>
+        <button
+          onClick={() => {
+            withdraw(0);
+          }}
+          data-testid="withdraw"
+        >
+          Withdraw
+        </button>
 
-  return (
-    <>
-      <button
-        data-testid="allocate"
-        onClick={() =>
-          allocate([
-            {
-              address: '0xee4abd006630aea6fa3e685c99506db31c09b3f4',
-              share: 500,
-            },
-            {
-              address: '0x21b2be9090d1d319e57b67c4b5d37bc5ec29a9d0',
-              share: 500,
-            },
-          ])
-        }
-      >
-        Allocate Shares
-      </button>
-      {allocationData && (
-        <>
-          <span data-testid="allocation"></span>
-          <Withdraw address={address} />
-        </>
-      )}
-    </>
-  );
-}
-
-function Setup({ address }: { address: string }) {
-  const nft = useNFT(address);
-  const { setup, data: revenueShareData } = useSetupRevenueSharing(nft);
-
-  return (
-    <>
-      <button
-        data-testid="setupRevenueSharing"
-        onClick={() => {
-          setup({
-            revShareExtensionAddress:
-              '0x483C3aDD26C87d2F99DcCB84Cbf61844B6aeD212',
-            collaborators: [
-              {
-                address: '0xee4abd006630aea6fa3e685c99506db31c09b3f4',
-                share: 1000,
-              },
-              {
-                address: '0x21b2be9090d1d319e57b67c4b5d37bc5ec29a9d0',
-                share: 1000,
-              },
-            ],
-            holderPercentage: 5000,
-          });
-        }}
-      >
-        Setup Revenue Sharing
-      </button>
-
-      {revenueShareData && (
-        <>
-          <div data-testid="revenueShareData"></div>
-          <Allocation address={address} />
-        </>
-      )}
-    </>
-  );
-}
-
-describe('useRevenueSharingAllocation', () => {
-  it('allows you to allocate shares', async () => {
-    render(
-      <DeployedTest>
-        {({ address }) => <Setup address={address} />}
-      </DeployedTest>
+        {data && <span data-testid="status">{data.status}</span>}
+      </>
     );
+  }
 
-    const setup = await waitFor(() =>
-      screen.getByTestId('setupRevenueSharing')
-    );
-
-    setup.click();
-    // @TODO gets stuck here
-    await waitFor(() => screen.getByTestId('revenueShareData'));
-
-    screen.getByTestId('allocate').click();
-    await waitFor(() => screen.getByTestId('allocation'));
-
+  it('allows withdrawal of token funds', async () => {
+    render(<Withdraw />);
     screen.getByTestId('withdraw').click();
-    await waitFor(() => screen.getByTestId('withdrawal'));
+    const status = await waitFor(() => screen.getByTestId('status'));
+    expect(status).toHaveTextContent('1');
   });
 });
