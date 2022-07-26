5c45462b3d4de322961643df810be10e701f20b7
Andy <andy.kitt@simpleweb.co.uk>
2022-07-26 16:26:18 +0100

    Add transaction arguments to node-deploy example

--- examples/node-deploy/index.js
+++ examples/node-deploy/index.js
@@ -1,4 +1,5 @@
 import { OpenFormatSDK } from "@simpleweb/open-format";
+import { ethers } from "ethers";
 
 const sdk = new OpenFormatSDK({
   network: "mumbai",
@@ -7,13 +8,21 @@ const sdk = new OpenFormatSDK({
 
 console.log("Deploying...");
 
-const { transactionHash } = await sdk.deploy({
-  maxSupply: 100,
-  mintingPrice: 0.01,
-  name: "Test 1",
-  symbol: "TEST1",
-  url: "ipfs://"
-});
+const { transactionHash } = await sdk.deploy(
+  {
+    maxSupply: 100,
+    mintingPrice: 0.01,
+    name: "Test 1",
+    symbol: "TEST1",
+    url: "ipfs://"
+  }
+  // @dev uncomment to pass through transaction arguments.
+  // https://docs.ethers.io/v5/api/utils/transactions/#Transaction
+  // {
+  //   maxFeePerGas: ethers.BigNumber.from(60e9),
+  //   maxPriorityFeePerGas: ethers.BigNumber.from(60e9)
+  // }
+);
 
 console.log(
   `View on Polygonscan: https://mumbai.polygonscan.com/tx/${transactionHash}`
