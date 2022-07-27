5c3a342dacca8959f21637c2f160da64443ef5cb
Craig Coles <craig@simpleweb.co.uk>
2022-07-27 14:24:48 +0100

    Allows `useDeploy` to accept transaction arguments

--- sdks/react/src/hooks/useDeploy.tsx
+++ sdks/react/src/hooks/useDeploy.tsx
@@ -1,19 +1,21 @@
 import { useMutation } from 'react-query';
+import { Transaction } from 'ethers';
 import { useOpenFormat } from '../provider';
 
 /**
  * Gets the deploy function from the sdk
+ * @param {Transaction} [transactionArgs]
  * @returns deploy function
  */
-export function useDeploy() {
+export function useDeploy(transactionArgs?: Transaction) {
   const { sdk } = useOpenFormat();
 
   const { mutateAsync, ...mutation } = useMutation<
     Awaited<ReturnType<typeof sdk.deploy>>,
     unknown,
-    Parameters<typeof sdk.deploy>
+    Parameters<typeof sdk.deploy>[0]
   >(data => {
-    return sdk.deploy(...data);
+    return sdk.deploy(data, transactionArgs);
   });
 
   return {
