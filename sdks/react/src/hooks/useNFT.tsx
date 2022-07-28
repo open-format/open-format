de5e0a53c063098da2cce425aa2f27f72cf65e1d
Benjamin Reid <ben@simpleweb.co.uk>
2022-07-28 10:09:49 +0100

    Allow signer to be optional in the NFT class so it can be added after connecting to a wallet

--- sdks/react/src/hooks/useNFT.tsx
+++ sdks/react/src/hooks/useNFT.tsx
@@ -1,7 +1,23 @@
-import { useRef } from 'react';
+import { useConnectWallet } from '@web3-onboard/react';
+import { ethers } from 'ethers';
+import { useEffect, useRef } from 'react';
 import { useOpenFormat } from '../provider';
 
 export function useNFT(address: string) {
+  const [{ wallet }] = useConnectWallet();
   const { sdk } = useOpenFormat();
-  return useRef(sdk.getNFT(address)).current;
+
+  const nft = useRef(sdk.getNFT(address));
+
+  useEffect(() => {
+    if (wallet) {
+      nft.current.signer = new ethers.providers.Web3Provider(
+        wallet.provider
+      ).getSigner();
+    } else {
+      nft.current.signer = undefined;
+    }
+  }, [wallet]);
+
+  return nft.current;
 }
