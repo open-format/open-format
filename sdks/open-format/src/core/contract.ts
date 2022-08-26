bbdf8ec6d01f297e7f5b06c089d32bb28ca43a85
RuariM <48099261+RuariM@users.noreply.github.com>
2022-08-26 16:32:44 +0100

    optional storage token argument added to deploy

--- sdks/open-format/src/core/contract.ts
+++ sdks/open-format/src/core/contract.ts
@@ -6,11 +6,6 @@ import { invariant } from '../helpers/invariant';
 import isZeroAddress from '../helpers/zeroAddress';
 import { NFTMetadata } from '../types';
 
-const nftStorage = new NFTStorage({
-  token:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDVkMjJDZDg2M2Y2NUVBN0ZjZjI3MEE5MUY2NTE5Nzc4OGRhRjU4NmMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1OTAxMTU3OTQ2NSwibmFtZSI6Ik9wZW4gRm9ybWF0In0.v-0FkXH0IpVB_JdyG6Ho6kXPqy4DOVr_HZzsks3DES4',
-});
-
 type ContractArgs = {
   contractAddress: string;
   signer: Signer;
@@ -420,6 +415,9 @@ export async function burn({
   return receipt;
 }
 
+const defaultNFTStorageToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDVkMjJDZDg2M2Y2NUVBN0ZjZjI3MEE5MUY2NTE5Nzc4OGRhRjU4NmMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1OTAxMTU3OTQ2NSwibmFtZSI6Ik9wZW4gRm9ybWF0In0.v-0FkXH0IpVB_JdyG6Ho6kXPqy4DOVr_HZzsks3DES4';
+
 /**
  * Deploys a version of the Open Format contract
  * @private
@@ -433,11 +431,13 @@ export async function deploy({
   nft,
   transactionArgs,
   factory,
+  nftStorageToken,
 }: {
   signer: Signer;
   nft: NFTMetadata;
   transactionArgs?: Transaction;
   factory?: string;
+  nftStorageToken?: string;
 }) {
   const openFormatContract = new ethers.ContractFactory(
     base.abi,
@@ -451,8 +451,15 @@ export async function deploy({
     invariant(nft.description, 'A description must be set');
     invariant(nft.image, 'An image must be set');
     invariant(nft.releaseType, 'A release type must be set');
+    //invariant(nftStorageToken, 'an NFTStorage api key must be provided');
 
     const customMetadata = nft.metadata ?? {};
+
+    console.log(nftStorageToken);
+
+    const nftStorage = new NFTStorage({
+      token: nftStorageToken || defaultNFTStorageToken,
+    });
 
     const metadata = await nftStorage.store({
       name: nft.name,
--- sdks/open-format/test/deploy.test.ts
+++ sdks/open-format/test/deploy.test.ts
@@ -73,4 +73,28 @@ describe('sdk.deploy()', () => {
       })
     ).rejects.toThrow();
   });
+
+  it('can deploy with an NFTStorage token', async () => {
+    const sdk = new OpenFormatSDK({
+      network: 'http://localhost:8545',
+      signer: new ethers.Wallet(
+        '0xc27786e23ac741aceef158731965a6285f350e114952201baad6149c18d735e7',
+        new ethers.providers.JsonRpcProvider('http://localhost:8545')
+      ),
+    });
+
+    const receipt = await sdk.deploy(
+      {
+        maxSupply: 100,
+        mintingPrice: 0.01,
+        name: 'Test',
+        symbol: 'TEST',
+        url: 'ipfs://',
+      },
+      undefined,
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEY0OUMxZjBBRTdmYjMxNjI1NDNmZkEwNTc1NkM1QzNFNzI5MjhEYzMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0ODAzNzcyOTMwNiwibmFtZSI6InRoZWZhY3RvcnkifQ._Jc515t7h-8-4tTrLUqAL5i3B4Zv2BKDcOjQnlIedgE'
+    );
+
+    expect(receipt.status).toBe(1);
+  });
 });
